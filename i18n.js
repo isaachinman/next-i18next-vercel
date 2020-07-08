c34a17d0c4cb4355e299a835736593feeb7cdd97
Isaac Hinman <isaac@isaachinman.com>
2020-07-08 10:28:47 +0100

    Add path.resolve to localePath

--- i18n.js
+++ i18n.js
@@ -1,7 +1,9 @@
+const path = require('path')
 const NextI18Next = require('next-i18next').default
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 
 module.exports = new NextI18Next({
   otherLanguages: ['de'],
   localeSubpaths,
+  localePath: path.resolve('./public/static/locales'),
 })
