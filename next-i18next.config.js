3cffd0708cb20751b9f0e6780315a1cc2ea68f52
Isaac Hinman <isaac@isaachinman.com>
2021-10-21 13:31:04 +0100

    next@canary

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
 module.exports = {
-  defaultLocale: 'en',
-  locales: ['en', 'de'],
-  localePath: path.resolve('./public/static/locales')
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+    localePath: path.resolve('./public/static/locales')
+  }
 }
